267613bf8d1809828adbd946fc66ac726e9cf5de
Brian Lee <brianlee@Brian-Lees-MacBook-Air.local>
2024-11-20 11:58:03 +0900

    game over function complete

--- src/state_check.cpp
+++ src/state_check.cpp
@@ -1,4 +1,15 @@
 #include <game.hpp>
 bool game_over(int board[4][4]){
-    return 0;
+    bool is_game_over = true;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == board[i][j+1]) {
+                is_game_over == false;
+            }
+            if (board[j][i] == board[j+1][i]) {
+                is_game_over == false;
+            }
+        }
+    }
+    return is_game_over;
 }
