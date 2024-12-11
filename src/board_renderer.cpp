27cac5eab1a1ca7c4f2e6541229b31879fb74116
Leah Hong <leahhong@Leahs-MacBook-Air.local>
2024-12-11 11:52:40 +0900

    implements render_board

--- src/board_renderer.cpp
+++ src/board_renderer.cpp
@@ -1,4 +1,31 @@
 #include <game.hpp>
-void render_board(int board[4][4]){
+void render_board(int board[4][4])
+{
     new_screen();
+    for(int i = 0; i<4; i++ )
+    {
+        for (int j=0;j<4;j++)
+        {
+            output(std::to_string(board[i][j])+" ");
+        }
+        output("\n");  
+    }
+
 }
+// std::to_string(7) -> "7"
+/*
+0 0 0 0
+2 0 4 0
+0 2 0 2
+2 2 2 0
+
+output("0 ")
+output("0 ")
+output("0 ")
+output("0 ")
+output("2 ")
+output("0 ")
+output("4 ")
+output("0 ")
+*/
+
